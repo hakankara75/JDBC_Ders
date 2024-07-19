98abf24a1f880d32d08040a3e1b8a591b4d6c231
hakankara75 <hakanbatirhan@gmail.com>
2024-07-19 21:21:40 +0300

    v

--- src/test/java/firstTest/Query09.java
+++ src/test/java/firstTest/Query09.java
@@ -29,7 +29,7 @@ public class Query09 {
     }
 
     @Test
-    public void name() {
+    public void positiveTest2() {
 
         String hostName="localhost";
         String dbName="Deneme";
@@ -54,10 +54,57 @@ public class Query09 {
         }catch (Exception exception){
 
         }
-
-
         assertTrue("Deneme databesinde aranan değer bulunamadı",counter>0);
 
         closeConnectionAndStatement();
+    }
+
+    @Test
+    public void negativeTest1() {
+        createConnection();
+
+        String sql="select * from ogrenci_bilgiler";
+        String column="kulup_calismasi";
+        String query="Binicilik";
+        assertTrue("Database'de "+query+"'e ait veri bulunamamıştır.", getColumnData(sql,column).contains(query));
+
+        closeConnection();
+
+
+    }
+
+
+    @Test
+    public void negativeTest2() {
+        String hostName="localhost";
+        String databaseName="Deneme";
+        String userName="postgres";
+        String password="123456";
+        connectToDataBase(hostName,databaseName,userName,password);
+
+        createStatement();
+
+        String sql="select * from ogrenci_bilgiler";
+        String query="Binicilik";
+
+        ResultSet resultSet=JdbcUtils.executeQuery(sql);
+
+        int counter=0;
+        try {
+            while (resultSet.next()){
+                System.out.println("Kulup Çalışmaları sütunundaki veriler = " + resultSet.getString(3));
+                if (resultSet.getString(3).contains(query)){
+                    counter++;
+                }
+            }
+
+        }catch (Exception exception){
+
+        }
+
+        Assert.assertTrue(counter>0, "Database'de "+query+"'e ait veri bulunamamıştır.");
+
+        closeConnectionAndStatement();
+
     }
 }
